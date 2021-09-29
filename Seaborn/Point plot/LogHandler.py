392531485baf7b2cf1ac9be039d76bc568301a30
“Samarth <samarthbm18@gmail.com>
2021-09-29 15:37:03 +0530

    [Samarth] Add: Log handler file.

--- Seaborn/Point plot/LogHandler.py
+++ Seaborn/Point plot/LogHandler.py
@@ -3,7 +3,7 @@ import logging
 logger = logging
    
 # logging basic config method and saving log files
-logger.basicConfig(filename='Seaborn_barchart.log', level=logging.INFO,
+logger.basicConfig(filename='pointplot.log', level=logging.INFO,
                     format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
-logger.basicConfig(filename='Seaborn_barchart.log', level=logging.ERROR,
+logger.basicConfig(filename='pointplot.log', level=logging.ERROR,
 format='%(asctime)s:%(funcName)s:%(levelname)s:%(lineno)d')
